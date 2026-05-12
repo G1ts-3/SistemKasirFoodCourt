3981d92751ec9a16b29e3a2bc27b09a57a169a4c
Git <raghidmuhammad3@gmail.com>
2026-05-12 13:07:24 +0700

    first commit

---
+++ src/main/java/WG58/Pengguna.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package WG58;
+
+/**
+ *
+ * @author hi
+ */
+public abstract class Pengguna {
+
+    private String idPengguna;
+    private String peran;
+
+    public abstract boolean aksesSistem();
+}
